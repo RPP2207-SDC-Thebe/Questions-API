b7058dc3af3aeab9416a3b557ea0a3f861a8c477
Jason Kwong <skjKwong@gmail.com>
2023-01-23 23:07:20 -0800

    updated pool connect with client query to pull pid to verify if db is connected

--- server/db/index.js
+++ server/db/index.js
@@ -13,17 +13,30 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   // user: 'saikitJK'
   user: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD
-  // max: 40,
-  // idleTimeoutMillis: 30000,
-  // connectionTimeoutMillis: 2000,
-
+  password: process.env.DB_PASSWORD,
+  max: 40,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000
 })
 
-pool.connect()
-  .then(() => console.log(`connected to DB ${process.env.DB_NAME} as ${process.env.DB_USERNAME} via ${process.env.DB_HOST}:${process.env.PG_PORT}.`))
-  // .then(() => pool.qeury('select * from questions limit 5;'))
-  // .then((result) => console.table(result.rows))
-  .catch((err) => { console.log('error connecting DB: ', err) })
+// pool.connect()
+//   .then(() => console.log(`connected to DB ${process.env.DB_NAME} as ${process.env.DB_USERNAME} via ${process.env.DB_HOST}:${process.env.PG_PORT}.`))
+//   // .then(() => pool.qeury(`SELECT pid FROM pg_stat_activity where usename = "${process.env.DB_USERNAME}"`))
+//   // .then((result) => console.log(result.rows))
+//   .catch((err) => { console.log('error connecting DB: ', err) })
+
+pool.connect((err, client, release) => {
+  if (err) {
+    return console.error('Error acquiring client', err.stack)
+  }
+  client.query(`SELECT pid FROM pg_stat_activity where usename = '${process.env.DB_USERNAME}'`, (err, result) => {
+    release()
+    if (err) {
+      return console.error('Error executing query', err.stack)
+    }
+    console.log(`Connected to DB ${process.env.DB_NAME} as ${process.env.DB_USERNAME} via ${process.env.DB_HOST}:${process.env.PG_PORT} on PID:${result.rows[0].pid}.`)
+
+  })
+})
 
 module.exports = pool;
