6ff7ac229421ff5d3c38654697c05c3c4eaadf66
Jason Kwong <skjKwong@gmail.com>
2023-02-06 14:43:50 -0800

    wrote stress tests

---
+++ testing/putQuestionReport_stress_test.js
@@ -0,0 +1,20 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+
+export const options = {
+
+  insecureSkipTLSVerify: true,
+  noConnectionReuse: false,
+  vus: 10000,
+  duration: '30s'
+}
+
+export default function putQuestionReport() {
+  const baseURL = 'http://localhost:8080/qa'
+  //max question_id: 3518963
+  let randomID = Math.floor(Math.random() * (3518963 - 1) + 1)
+  http.put(`${baseURL}/questions/${randomID}/report`);
+  sleep(1);
+
+}
---
+++ testing/server_stress_test.js
@@ -0,0 +1,20 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+
+export const options = {
+  insecureSkipTLSVerify: true,
+  noConnectionReuse: false,
+  vus: 10000,
+  duration: '30s'
+}
+
+export default function test() {
+  const baseURL = 'http://localhost:8080'
+  http.get(`${baseURL}/test`);
+  sleep(1);
+}
+
+
+
+
